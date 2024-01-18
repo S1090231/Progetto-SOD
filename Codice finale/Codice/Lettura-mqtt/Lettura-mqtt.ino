a2695e172ff43569cac7014f54f0ba79ab15a051
Giada <s1108648@studenti.univpm.it>
2024-01-18 17:25:29 +0100

    Aggiornato codice

--- Codice finale/Codice/Lettura-mqtt/Lettura-mqtt.ino
+++ Codice finale/Codice/Lettura-mqtt/Lettura-mqtt.ino
@@ -8,50 +8,51 @@
 #include <PubSubClient.h>
 #include <BH1750.h>
 
+RTC_PCF8523 rtc;
+TaskHandle_t TaskBMP280;
+TaskHandle_t TaskBH1750;
+TaskHandle_t TaskRTC;
+TaskHandle_t TaskMQTT; 
+
+AsyncWebServer server(80);
+
+Adafruit_BMP280 bmp;
+BH1750 lightMeter;
+
+
+const char* ssid = "TP-Link_BB96";
+const char* password = "Casagatti1";
+const char* mqtt_server = "192.168.1.110";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-Adafruit_BMP280 bmp;
-BH1750 lightMeter;
-RTC_PCF8523 rtc;
-//char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
-
-TaskHandle_t TaskBMP280;
-TaskHandle_t TaskBH1750;
-TaskHandle_t TaskRTC;
-
-float bmpTemperature, bmpPressure, bh1750Lux;
-//String timestamp;
-
-void readBMP280Data(){
-  float temperature, pressure;
-  temperature = bmp.readTemperature();
-  pressure = bmp.readPressure () / 100.0F;
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.print("°C, Pressure: ");
-  Serial.print(pressure);
-  Serial.println("hPa");
-  //timestamp = getTimestamp();
-
-  String payload = " Temperature: " + String(temperature) + "°C, Pressure: " + String(pressure) + "hPa";
-  client.publish("sensor_data", payload.c_str());
+void reconnect(){
+  while(!client.connected()){
+    Serial.print("Prova connessione MQTT...");
+    if(client.connect("ESP32Client")){
+      Serial.println("Connesso!");
+      //client.subscribe("sensor_data");
+    }else{
+      Serial.print("Fallito, rc=");
+      Serial.print(client.state());
+      Serial.println("Ritenta tra 5 secondi");
+      delay(5000);
+    }
+  }
 }
 
-void readBH1750Data(){
-  float lux = lightMeter.readLightLevel();
-  Serial.print("Light intensity: ");
-  Serial.print(lux);
-  Serial.println("lux");
-  //timestamp = getTimestamp();
-
-  String payload = " Light intensity: " +String(lux) + "lux";
-  client.publish("sensor_data", payload.c_str());
+void mqttTask(void *pvParameters){
+  while(1){
+    if(!client.connected()){
+      reconnect();
+    }
+    client.loop();
+    vTaskDelay(1000/ portTICK_PERIOD_MS);
   }
- 
-  void readRTCData(void *pvParameters){
+}
+
+void readRTCData(void *pvParameters){
     TickType_t lastWakeTime = xTaskGetTickCount();
 
     while(1){
@@ -64,8 +65,8 @@ void readBH1750Data(){
        Serial.print(now.day(), DEC);
        Serial.print("-");
 
-    //Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
+    
+    Serial.print("- ");
     Serial.print(now.hour(), DEC);
     Serial.print(':');
     Serial.print(now.minute(), DEC);
@@ -74,62 +75,36 @@ void readBH1750Data(){
     Serial.println();
 
     vTaskDelayUntil(&lastWakeTime, 1000 / portTICK_PERIOD_MS);
-    }}
-
-
-  //Funzione per ottenere il timestamp dal modulo RTC
-  /*String getTimestamp(){
-    DateTime now = rtc.now();
-    String timestamp = String(now.year()) + "-" + String(now.month()) + "-" + String(now.day()) + " " +
-                       String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second());
-    return timestamp;                   
-  }*/
-
-  //Funzione per gestire la richiesta HTTP
-  void handleRoot(AsyncWebServerRequest *request){
-    String html = "<html><body";
-    html += "<h1>Sensor Data</h1>";
-    html += "<p>BMP280 Temperature: " +String(bmpTemperature)+ "°C </p>";
-    html += "<p>BMP280 Pressure: " +String(bmpPressure)+ "hPa </p>";
-    html += "<p>BH1750 Light Intensity: " +String(bh1750Lux)+ "lux </p>";
-    html += "</html></body>";
-
-    request->send(200, "text/html", html);
-  }
-
-  void TaskBMP280code(void *pvParameters){
-    (void) pvParameters;
-    for(;;){
-      readBMP280Data();
-      delay(5000);
     }
   }
 
-  void TaskBH1750code(void *pvParameters){
-    (void) pvParameters;
-    for(;;){
-      readBH1750Data();
-      delay(5000);
-    }
-  }
-
-void reconnect(){
-  while(!client.connected()){
-    Serial.print("Prova connessione MQTT...");
-    if(client.connect("ESP32Client")){
-      Serial.println("Connesso!");
-      client.subscribe("sensor_data");
-    }else{
-      Serial.print("Fallito, rc=");
-      Serial.print(client.state());
-      Serial.println("Ritenta tra 5 secondi");
-      delay(5000);
-    }
-  }
+  void readBMP280Data(void *pvParameters){
+    while(1){
+  Serial.print("Temperature: ");
+  Serial.print(bmp.readTemperature());
+  Serial.print("°C, Pressure: ");
+  Serial.print(bmp.readPressure() / 100.0F);
+  Serial.println("hPa");
+  
+   vTaskDelay(5000 / portTICK_PERIOD_MS);
 }
+  }
 
-void setup() {
+  void readBH1750Data(void *pvParameters){
+    while(1){
+  float lux = lightMeter.readLightLevel();
+  Serial.print("Light intensity: ");
+  Serial.print(lux);
+  Serial.println("lux");
+
+  vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+  }
+  }
+
+  void setup() {
   Serial.begin(115200);
+  Wire.begin();
 
   if(!bmp.begin()){
     Serial.println("Errore inizializzazione BMP280");
@@ -147,55 +122,66 @@ void setup() {
   }
 
   
-  WiFi.begin("TP-Link_BB96", "Casagatti1" );
+  WiFi.begin(ssid, password );
   while(WiFi.status() != WL_CONNECTED) {
-    delay(1000);
+    delay(250);
     Serial.println("Connessione WiFi in corso...");
   }
   Serial.println("WiFi Connesso");
   Serial.println(WiFi.localIP());
 
-
-
-  client.setServer("192.168.1.105", 1883);
+  client.setServer(mqtt_server, 1883);
 
   xTaskCreatePinnedToCore(
-    TaskBMP280code,
+    readBMP280Data,
     "TaskBMP280",
-    20000,
+    10000,
     NULL,
     1,
     &TaskBMP280,
-    0);
+    1);
 
     xTaskCreatePinnedToCore(
-    TaskBH1750code,
+    readBH1750Data,
     "TaskBH1750",
-    20000,
+    10000,
     NULL,
     1,
     &TaskBH1750,
-    0);
+    1);
 
     xTaskCreatePinnedToCore(
     readRTCData,
     "TaskRTC",
-    20000,
+    10000,
     NULL,
     1,
     &TaskRTC,
     0);
 
+    xTaskCreatePinnedToCore(
+    mqttTask,
+    "TaskMqtt",
+    10000,
+    NULL,
+    1,
+    &TaskMQTT,
+    1);
+
     //Inizializzazione Server Web
-    AsyncWebServer server(1883);
-    server.on("/", HTTP_GET, handleRoot);
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+      DateTime now = rtc.now();
+          String html = "<html><body>";
+          html += "<h1>Sensor data</h1>";
+          html += "<p>Timestamp RTC: " + String(now.year()) + "-" + String(now.month()) + "-" + String(now.day()) + 
+                  " " + String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second()) + "</p>";
+          html += "<p> Temperatura BMP280: " + String(bmp.readTemperature()) + "&deg;C </p>";
+          html += "<p> Pressione BMPP280 : " + String(bmp.readPressure()/100.0F) + "hPa </p>";
+          html += "<p> Luminosità BH1750: " + String(lightMeter.readLightLevel()) + "lux </p>";
+          html += "</body></html>"; 
+          request->send(200, "text/html", html);
+    });
     server.begin();
 }
 
-void loop() {
-  if(!client.connected()){
-    reconnect();
-  }
-  client.loop();
-
-}
+void loop(){}
